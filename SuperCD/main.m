598e6bcdda74a675c4c1c3f678431603c80ec592
Corezina <corezina@corezinas-imac.local>
2016-02-05 03:51:39 -0800

    Initial Commit

---
+++ SuperCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SuperCD
+//
+//  Created by Corezina on 2/5/16.
+//  Copyright © 2016 Corezina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
